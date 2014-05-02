1872c50c2c4ec6d276cda183b1939c17660c941f
Tim Bugai <tim@bugai.net>
2014-05-02 18:55:30 -0400

    Initial Commit

---
+++ Harmony/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Harmony
+//
+//  Created by Tim Bugai on 5/2/14.
+//  Copyright (c) 2014 Collective Idea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Harmony/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Harmony
+//
+//  Created by Tim Bugai on 5/2/14.
+//  Copyright (c) 2014 Collective Idea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
